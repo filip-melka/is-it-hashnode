ecea8873ace2340a91fa36dd5f860d9a71b223a2
its-filip-melka <133359834+its-filip-melka@users.noreply.github.com>
2024-06-04 16:26:11 +0200

    Disable popup

--- background.js
+++ background.js
@@ -1,5 +1,17 @@
 chrome.runtime.onMessage.addListener((req) => {
 	if (req.action === 'changeIcon') {
 		chrome.action.setIcon({ path: req.iconPath })
+
+		if (req.iconPath.includes('disabled')) {
+			chrome.tabs.query(
+				{ active: true, windowType: 'normal', currentWindow: true },
+				function (d) {
+					chrome.action.setPopup({
+						tabId: d[0].id,
+						popup: '',
+					})
+				}
+			)
+		}
 	}
 })
